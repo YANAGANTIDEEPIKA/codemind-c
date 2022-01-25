70f9f5d34e08dcdc0655e8a0343af4085b7eca8b
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-01-25 16:53:17 +0530

    #include<stdio.h> int main() {     char z,a,e,i,o,u,A,E,I,O,U;     scanf("%c",&z);     if(z=='a' || z=='e' || z=='i' || z=='o' || z=='u' || z=='A' || z=='E' || z=='I' || z=='O' || z=='U')     {         printf("Vowel");     }     else{         printf("Consonant");     }     return 0; }

--- Program_to_check_Vowel_or_Consonant.c
+++ Program_to_check_Vowel_or_Consonant.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    char c,a,e,i,o,u,A,E,I,O,U;
-    scanf("%c",&c);
-    if(c=='A' || c=='E' || c=='I' || c=='O' ||c=='U' || c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+    char z,a,e,i,o,u,A,E,I,O,U;
+    scanf("%c",&z);
+    if(z=='a' || z=='e' || z=='i' || z=='o' || z=='u' || z=='A' || z=='E' || z=='I' || z=='O' || z=='U')
     {
         printf("Vowel");
     }
     else{
-        printf("Consonant");a;
-}
+        printf("Consonant");
+    }
+    return 0;
 }
