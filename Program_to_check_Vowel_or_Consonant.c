1337b2bc1b01cd725446de3af61846fc3c307f69
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-01-25 16:32:51 +0530

    #include<stdio.h> int main() {     char c,a,e,i,o,u,A,E,I,O,U;     scanf("%c",&c);     if(c=='A' || c=='E' || c=='I' || c=='O' ||c=='U' || c=='a' || c=='e' || c=='i' || c=='o' || c=='u')     {         printf("Vowel");     }     else{         printf("Consonant");a; } }

---
+++ Program_to_check_Vowel_or_Consonant.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char c,a,e,i,o,u,A,E,I,O,U;
+    scanf("%c",&c);
+    if(c=='A' || c=='E' || c=='I' || c=='O' ||c=='U' || c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+    {
+        printf("Vowel");
+    }
+    else{
+        printf("Consonant");a;
+}
+}
