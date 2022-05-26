18d0dcf867e84cf983643ef51389501432633577
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-05-26 13:03:26 +0530

    #include<stdio.h> int ugly(int n) {     if(n==1)     return 1;     if(n<=0)     return 0;     if(n%2==0)     return (ugly(n/2));     if(n%3==0)     return (ugly(n/3));      if(n%5==0)     return (ugly(n/5));     return 0; } int main() {     int n;     scanf("%d",&n);     if(ugly(n)==1)     printf("Ugly Number");     else     printf("Not Ugly Number"); }

---
+++ Ugly_number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int ugly(int n)
+{
+    if(n==1)
+    return 1;
+    if(n<=0)
+    return 0;
+    if(n%2==0)
+    return (ugly(n/2));
+    if(n%3==0)
+    return (ugly(n/3));
+     if(n%5==0)
+    return (ugly(n/5));
+    return 0;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(ugly(n)==1)
+    printf("Ugly Number");
+    else
+    printf("Not Ugly Number");
+}
