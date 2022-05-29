bb972519387d025724d6f6065fa64897fae9beff
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-05-29 17:10:12 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             printf("%c ",'A'+i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,12 +3,11 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        for(j=1;j<=n;j++){
-            if(i==j)
-            printf("0");
-            else
-            printf("x");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%c ",'A'+i);
         }
         printf("
 ");
