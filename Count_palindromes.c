6c39158bf17a41f4894097f82f8fab85ce10dab8
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-07-03 12:59:30 +0530

    #include<stdio.h> int pallin(int n) {     int re=0,r;     while(n!=0)     {         r=n%10;         re=re*10+r;         n/=10;     }     return re; } int main() {     int n,c=0;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]==pallin(a[i]))         {             c++;         }     }     printf("%d",c); }

---
+++ Count_palindromes.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int pallin(int n)
+{
+    int re=0,r;
+    while(n!=0)
+    {
+        r=n%10;
+        re=re*10+r;
+        n/=10;
+    }
+    return re;
+}
+int main()
+{
+    int n,c=0;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==pallin(a[i]))
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
